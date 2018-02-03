5bcb70eb26e30975a4ebd44265db3c0514e760fb
Parjinder Pannu <ppannu@cisco.com>
2018-02-03 12:38:32 -0800

    Anno setter demo

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -11,9 +11,15 @@ import com.pluralsight.repository.CustomerRepository;
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 	
-	@Autowired
+	//@Autowired
 	private CustomerRepository customerRepository;
 	
+	@Autowired
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		System.out.println("We are using setter injection");
+		this.customerRepository = customerRepository;
+	}
+
 	/* (non-Javadoc)
 	 * @see com.pluralsight.service.CustomerService#findAll()
 	 */
